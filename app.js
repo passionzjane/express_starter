b988ba993f221e7f3de0ec311f1f7f45bbc90271
JaneStack <mrumunjanet@gmail.com>
2021-11-01 14:23:32 -0700

    initial server set up

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express')
-
+const path = require('path')
+const dotenv = require('dotenv')
 
 const app = express()
 
 
+//Configure .dotenv
+dotenv.config({path: './config.env'});
 
-const port = 5000
+
+//Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// app.get('/', (req, res ) => {
+//     res.send('Running')
+// })
+
+
+
+const port = process.env.PORT || 3001
 
 const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
+
